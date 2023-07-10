9db5eda71051bdf7a93981d8376e2747a8fe9d43
chndnbhrdwj <chndnbhrdwj@users.noreply.github.com>
2023-07-10 17:00:02 +0100

    Update checkEnvironment.sh

--- checkEnvironment.sh
+++ checkEnvironment.sh
@@ -1,6 +1,13 @@
 #!/bin/zsh
 source environment.sh
 
+directory="logs";
+
+if [ ! -d "$directory" ]; then
+    mkdir -p "$directory"
+    echo "Directory '$directory' created."
+fi
+
 if [ -z $ENV ];
 then
     echo "USAGE: set the environment first"
