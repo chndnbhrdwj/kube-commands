4c7d68244292835e3ab10e0cad9beb6af5cc53bb
chndnbhrdwj <chndnbhrdwj@users.noreply.github.com>
2023-07-10 21:04:22 +0100

    Update copyLogs.sh

--- copyLogs.sh
+++ copyLogs.sh
@@ -9,9 +9,11 @@ else
 	if [ -z $2 ];
 	then
 	echo "No since time provided"
-	kubectl -n $ENV logs --all-containers=true $1 | grep -v 'metrics in the last' | grep -v 'admin/healthcheck' > logs/$1.log
+	kubectl -n $ENV logs --all-containers=true $1 | grep -v 'metrics in the last' | grep -v 'admin/healthcheck' |sed 's/\\t/	/g' | sed 's/\\n/\
+/g' > logs/$1.log
 	else
 	echo "Pulling logs since $2"
-	kubectl -n $ENV logs --all-containers=true $1 --since=$2 | grep -v 'metrics in the last' | grep -v 'admin/healthcheck' > logs/$1.log
+	kubectl -n $ENV logs --all-containers=true $1 --since=$2 | grep -v 'metrics in the last' | grep -v 'admin/healthcheck' | sed 's/\\t/	/g' | sed 's/\\n/\
+/g' > logs/$1.log
 	fi
 fi
