1f414c4d9701d8a96cedf3c9b0fea76005315ccc
chndnbhrdwj <chndnbhrdwj@users.noreply.github.com>
2023-07-10 16:53:33 +0100

    Update listPods.sh

--- listPods.sh
+++ listPods.sh
@@ -4,7 +4,7 @@ source checkEnvironment.sh
 
 if [ -z $1 ];
 then
-	kubectl -n $ENV get pods 
+	kubectl -n $ENV get pods | grep -v 'Completed'
 else
 	kubectl -n $ENV get pods | grep $1 | grep -v 'Completed'
 fi
